2bf6eedc6e2f25bf6fa13ac92adda6bfdd7d639b
AJMA <ajmoralesartacho@gmail.com>
2019-10-26 19:51:59 +0200

    create main script to run

--- calibgauge/calib.py
+++ calibgauge/calib.py
@@ -26,5 +26,4 @@ def get_calfactors(gauge, cal_date):
 
     return lm
 
-get_calfactors(gauge=2130917245, cal_date=24082018)
-get_calfactors(gauge=2130917246, cal_date=24082018)
+
---
+++ main.py
@@ -0,0 +1,6 @@
+
+
+from calibgauge.calib import get_calfactors
+
+get_calfactors(gauge=2130917245, cal_date=24082018)
+get_calfactors(gauge=2130917246, cal_date=24082018)
